8224c7f22fe6313af211f4a78d6e689090cd913f
Steven Fraser <frase1sr@cmich.edu>
2017-10-30 15:36:58 -0400

    Data models are mostly completed (subject to change)

--- Assets/Code/SurvivorMain.cs
+++ Assets/Code/SurvivorMain.cs
@@ -9,11 +9,22 @@ public class SurvivorMain : MonoBehaviour {
 	void Start () {
         var game = new SurvivorGameModel();
         var player = new SurvivalPlayerModel();
-       
+        var AI = new AttackerAIModel(AIType.Easy);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    void GenerateMap(SurvivorGameModel game)
+    {
+        Random random = new Random();
+        var width = game.MapWidth;
+        var length = game.MapLength;
+        foreach (var obj in game.Objects)
+        {
+            Vector3 position = new Vector3(Random.Range(-10.0f,width), 0, Random.Range(-10.0f, length));
+        }
+    }
 }
--- Assets/Models/SurvivorGameModel.cs
+++ Assets/Models/SurvivorGameModel.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Models
 {
 
     public class SurvivorGameModel
     {
+        public const int NumberOfObjects = 10;
         public int SpawnRate { get; set; }
         public List<PlayerModel> Players { get; set; }
+        public List<GameObject> Objects { get; set; }
+
+        public int MapLength { get; set; }
+        public int MapWidth { get; set; }
         public SurvivorGameModel()
         {
             SpawnRate = 1;
             Players = new List<PlayerModel>();
+            this.MapWidth = 50;
+            this.MapLength = 50;
+
         }
     }
 }
